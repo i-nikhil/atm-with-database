364d2e81b13b8612ef548d756f1b27673965bae7
NIKHIL PRAKASH <nikhilprakash510@gmail.com>
2021-03-30 15:44:46 +0530

    Add files via upload

---
+++ src/test/java/com/example/ATMStimulationSoftware/AtmStimulationSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ATMStimulationSoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtmStimulationSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
